641b1a0ea9a1be80fa67bf51c44503d461accfce
ishi0n <guibs225@gmail.com>
2022-04-24 04:13:49 -0300

    feat: remove linha caso não esteja ouvindo nada no momento

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import requests
 import tweepy
 from dotenv import load_dotenv
 
+
 def get_current_song(user: str, api_key: str) -> str:
     response = requests.get(f'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json')
     response_json = response.json()
@@ -17,25 +18,32 @@ def get_current_song(user: str, api_key: str) -> str:
         if '@attr' in track.keys() and track['@attr']['nowplaying']:
             return f'{track["artist"]["#text"]} - {track["name"]}'
 
+
 load_dotenv()
 
+auth = tweepy.OAuth1UserHandler(os.getenv('TWITTER_CONSUMER_KEY'), os.getenv('TWITTER_CONSUMER_SECRET_KEY'),
+                                os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+api = tweepy.API(auth)
+
 while True:
-    actual_track = get_current_song(os.getenv('LASTFM_USER'), os.getenv('LASTFM_API_KEY'))
-
-    if not actual_track:
-        continue
-
-    print(f'[{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] listening to: '+actual_track)
-
-    auth = tweepy.OAuth1UserHandler(os.getenv('TWITTER_CONSUMER_KEY'), os.getenv('TWITTER_CONSUMER_SECRET_KEY'),
-                                    os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_TOKEN_SECRET'))
-    api = tweepy.API(auth)
+    actual_track = get_current_song(
+        os.getenv('LASTFM_USER'), os.getenv('LASTFM_API_KEY'))
     actual_description = api.update_profile().description
 
-    if re.search(r'listening to.*', actual_description):
-        new_description = re.sub(r'listening to.*', 'listening to '+actual_track, actual_description)
+    if not actual_track:
+        if not re.search(r'listening to.*', actual_description):
+            continue
+
+        new_description = re.sub(r'listening to.*', '', actual_description)
     else:
-        new_description = actual_description.strip() + '\nlistening to ' + actual_track
+        print(
+            f'[{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] listening to: '+actual_track)
+
+        if re.search(r'listening to.*', actual_description):
+            new_description = re.sub(
+                r'listening to.*', 'listening to '+actual_track, actual_description)
+        else:
+            new_description = actual_description.strip() + '\n\nlistening to ' + actual_track
 
     api.update_profile(description=new_description)
 
