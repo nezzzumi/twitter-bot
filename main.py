e2acf82dc789af581163a24487f313c22f0a8977
ishi0n <guibs225@gmail.com>
2022-05-31 20:26:54 -0300

    refactor: adiciona função log

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 
 
 def get_current_song(user: str, api_key: str) -> str:
-    response = requests.get(f'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json', timeout=6)
+    response = requests.get(
+        f'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json', timeout=6)
     response_json = response.json()
 
     tracks = response_json['recenttracks']['track']
@@ -17,6 +18,10 @@ def get_current_song(user: str, api_key: str) -> str:
     for track in tracks:
         if '@attr' in track.keys() and track['@attr']['nowplaying']:
             return f'{track["artist"]["#text"]} - {track["name"]}'
+
+
+def log(msg: str):
+    print(f'[{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] {msg}')
 
 
 load_dotenv()
@@ -29,9 +34,10 @@ last_track = ''
 
 while True:
     try:
-        actual_track = get_current_song(os.getenv('LASTFM_USER'), os.getenv('LASTFM_API_KEY'))
+        actual_track = get_current_song(
+            os.getenv('LASTFM_USER'), os.getenv('LASTFM_API_KEY'))
     except Exception as e:
-        print(f'[{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] Erro ao buscar música atual: ', e)
+        log('Erro ao buscar música atual: ' + e)
         continue
 
     if actual_track == last_track:
@@ -47,8 +53,7 @@ while True:
 
         new_description = re.sub(r'escutando .*', '', actual_description)
     else:
-        print(
-            f'[{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] escutando: '+actual_track)
+        log('Escutando: '+actual_track)
 
         if re.search(r'escutando .*', actual_description):
             new_description = re.sub(
@@ -58,7 +63,7 @@ while True:
     try:
         api.update_profile(description=new_description)
     except Exception as e:
-        print(f'[{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}] Erro ao atualizar descrição: ', e)
+        log('Erro ao atualizar descrição: ' + e)
         continue
 
     time.sleep(int(os.getenv('INTERVAL')))
